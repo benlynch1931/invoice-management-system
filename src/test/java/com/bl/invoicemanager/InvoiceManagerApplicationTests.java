abd6178957de421669bbaca0fe2fcfaf0bfcf0cd
Ben Lynch <ben.lynch1931@gmail.com>
2023-03-20 20:08:39 +0000

    Initialise repo and liquibase scripts

---
+++ src/test/java/com/bl/invoicemanager/InvoiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.invoicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
